96ca028149b1952991c16335c671504905f66077
MD011436 <MDido@copsmonitoring.com>
2024-07-19 12:53:37 -0400

    Initial test of SSO

--- Program.cs
+++ Program.cs
@@ -1,31 +1,52 @@
-using BlazorApp1.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+////using BlazorApp1.Data;
+////using Microsoft.AspNetCore.Components;
+////using Microsoft.AspNetCore.Components.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+////var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+////// Add services to the container.
+////builder.Services.AddRazorPages();
+////builder.Services.AddServerSideBlazor();
+////builder.Services.AddSingleton<WeatherForecastService>();
 
-var app = builder.Build();
+////var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+////// Configure the HTTP request pipeline.
+////if (!app.Environment.IsDevelopment())
+////{
+////    app.UseExceptionHandler("/Error");
+////    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+////    app.UseHsts();
+////}
+
+////app.UseHttpsRedirection();
+
+////app.UseStaticFiles();
+
+////app.UseRouting();
+
+////app.MapBlazorHub();
+////app.MapFallbackToPage("/_Host");
+
+////app.Run();
+using BlazorApp1;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace blazorserver_B2C
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
